bb54fbbf5b15444e12c8082895cd5c750b782e17
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-04-02 21:07:35 -0400

    Use links

--- app/components/q-p-viewer.js
+++ app/components/q-p-viewer.js
@@ -8,11 +8,21 @@ export default class QPViewerComponent extends Component {
     return this.router.currentRoute?.queryParams || {};
   }
 
-  inc = (qp) => {
+  nextQps = (qp) => {
     let existing = this.queryParams?.[qp] || 0;
+    return { ...this.queryParams, [qp]: existing };
+  };
 
-    this.router.transitionTo({
-      queryParams: { ...this.queryParams, [qp]: existing },
-    });
+  nextForQp = (qp) => {
+    return `/?${new URLSearchParams(this.nextQps(qp))}`;
+  };
+
+  inc = (qp, event) => {
+    event?.preventDefault();
+    // Does not work without adding qps to controller
+    // this.router.transitionTo({
+    //   queryParams: this.nextQps(qp),
+    // });
+    this.router.transitionTo(this.nextForQp(qp));
   };
 }
